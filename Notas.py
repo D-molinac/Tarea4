cf431e91f0db25523102fd0b8e624726da6db37e
Diego Molina <diego.molina.c01@mail.pucv.cl>
2020-09-06 03:15:00 -0300

    diccionario

---
+++ Notas.py
@@ -0,0 +1,3 @@
+#Diccionary
+notas = { 'nombres' : ['lucas','pedro','natalia','jonathan'] ,'apellidos' : ['trotacielos','parque','romana','joestar'],'cursos' : ['Programacion','Fisica'], 'notas' : [[5.5,6.],[3.,4.7],[6.7,6.3],[5.,5.]]}
+#tabla de notas
