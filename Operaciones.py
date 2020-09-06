d02e8465f58bafbc7db197a55f42be202bd0c857
Diego Molina <diego.molina.c01@mail.pucv.cl>
2020-09-05 23:41:55 -0400

    sumaresta

---
+++ Operaciones.py
@@ -0,0 +1,10 @@
+# variables
+[w,x,y,z] = [10,3,5.5,9.]
+
+#suma
+print(f' x mas z es igual a {x+z}')
+print(f' w mas y es igual a {w+y}')
+
+#resta
+print(f' w menos z es igual a {w-z}')
+print(f' x menos y es igual a {x-y}')
