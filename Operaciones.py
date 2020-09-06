443dd5fa4958422d499582c8ee63d79c33715eae
Diego Molina <diego.molina.c01@mail.pucv.cl>
2020-09-06 01:52:40 -0300

    por_div_resto

--- Operaciones.py
+++ Operaciones.py
@@ -2,9 +2,14 @@
 [w,x,y,z] = [10,3,5.5,9.]
 
 #suma
-print(f' x mas z es igual a {x+z}')
-print(f' w mas y es igual a {w+y}')
+print(f' {x} mas {w} es igual a {x+w}')
 
 #resta
-print(f' w menos z es igual a {w-z}')
-print(f' x menos y es igual a {x-y}')
+print(f' {w} menos {z} es igual a {w-z}')
+
+#multiplicación
+print(f' {y} por {z} es igual a {y*z}')
+
+# division
+print(f' {w} dividido {x} es igual a {w/x}')
+print(f' {w} dividido {x} es aproximadamente {w//x} de resto {w%x}')
