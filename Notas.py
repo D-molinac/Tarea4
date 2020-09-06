939d394ad7ff84fd6490c00054d18e0b8f3b74eb
Diego Molina <diego.molina.c01@mail.pucv.cl>
2020-09-06 03:15:33 -0300

    Tabla_notas

--- Notas.py
+++ Notas.py
@@ -1,3 +1,7 @@
 #Diccionary
 notas = { 'nombres' : ['lucas','pedro','natalia','jonathan'] ,'apellidos' : ['trotacielos','parque','romana','joestar'],'cursos' : ['Programacion','Fisica'], 'notas' : [[5.5,6.],[3.,4.7],[6.7,6.3],[5.,5.]]}
 #tabla de notas
+print (notas['nombres'][0] , notas['apellidos'][0], notas['cursos'][0] , notas['notas'][0][0],notas['cursos'][1] ,notas['notas'][0][1])
+print (notas['nombres'][1] , notas['apellidos'][1], notas['cursos'][0] , notas['notas'][1][0],notas['cursos'][1] ,notas['notas'][1][1])
+print (notas['nombres'][2] , notas['apellidos'][2], notas['cursos'][0] , notas['notas'][2][0],notas['cursos'][1] ,notas['notas'][2][1])
+print (notas['nombres'][3] , notas['apellidos'][3], notas['cursos'][0] , notas['notas'][3][0],notas['cursos'][1] ,notas['notas'][3][1])
